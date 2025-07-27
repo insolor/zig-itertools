4557bac4489fe93c4612f5b948127050f23fea45
insolor <2442833+insolor@users.noreply.github.com>
2025-07-27 20:43:19 +0300

    Fix testing

--- src/zig_itertools.zig
+++ src/zig_itertools.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-fn SliceIterator(comptime T: type) type {
+pub fn SliceIterator(comptime T: type) type {
     return struct {
         slice: []const T,
         index: usize = 0,
@@ -17,16 +17,6 @@ fn SliceIterator(comptime T: type) type {
             return self.slice[self.index];
         }
     };
-}
-
-test "SliceIterator" {
-    const array = [_]i32{ 1, 2, 3 };
-    var iterator = SliceIterator(i32).init(&array);
-
-    try std.testing.expectEqual(1, iterator.next().?);
-    try std.testing.expectEqual(2, iterator.next().?);
-    try std.testing.expectEqual(3, iterator.next().?);
-    try std.testing.expect(iterator.next() == null);
 }
 
 pub fn ChainIterator(comptime I1: type, comptime I2: type, comptime R: type) type {
@@ -69,38 +59,9 @@ pub fn ChainIterator(comptime I1: type, comptime I2: type, comptime R: type) typ
     };
 }
 
-test "ChainIterator" {
-    const array1 = [_]i32{ 1, 2, 3 };
-    var iterator1 = SliceIterator(i32).init(&array1);
-
-    const array2 = [_]i32{ 9, 8, 7 };
-    var iterator2 = SliceIterator(i32).init(&array2);
-
-    var chain = ChainIterator(@TypeOf(iterator1), @TypeOf(iterator2), i32)
-        .init(&iterator1, &iterator2);
-
-    try std.testing.expectEqual(1, chain.next().?);
-    try std.testing.expectEqual(2, chain.next().?);
-    try std.testing.expectEqual(3, chain.next().?);
-    try std.testing.expectEqual(9, chain.next().?);
-    try std.testing.expectEqual(8, chain.next().?);
-    try std.testing.expectEqual(7, chain.next().?);
-    try std.testing.expect(chain.next() == null);
-}
-
-const EmptyIterator = struct {
+pub const EmptyIterator = struct {
     fn next(self: @This()) ?void {
         _ = self;
         return null;
     }
 };
-
-test "ChainIterator empty" {
-    var iterator1 = EmptyIterator{};
-    var iterator2 = EmptyIterator{};
-
-    var chain = ChainIterator(@TypeOf(iterator1), @TypeOf(iterator2), void)
-        .init(&iterator1, &iterator2);
-
-    try std.testing.expect(chain.next() == null);
-}
---
+++ tests/tests.zig
@@ -0,0 +1,50 @@
+const std = @import("std");
+const testing = std.testing;
+const expectEqual = testing.expectEqual;
+const expectEqualDeep = testing.expectEqualDeep;
+const ArrayList = std.ArrayList;
+const allocator = std.testing.allocator;
+
+const itertools = @import("zig_itertools");
+const SliceIterator = itertools.SliceIterator;
+const ChainIterator = itertools.ChainIterator;
+const EmptyIterator = itertools.EmptyIterator;
+
+test "SliceIterator" {
+    const array = [_]i32{ 1, 2, 3 };
+    var iterator = SliceIterator(i32).init(&array);
+
+    try std.testing.expectEqual(1, iterator.next().?);
+    try std.testing.expectEqual(2, iterator.next().?);
+    try std.testing.expectEqual(3, iterator.next().?);
+    try std.testing.expect(iterator.next() == null);
+}
+
+test "ChainIterator" {
+    const array1 = [_]i32{ 1, 2, 3 };
+    var iterator1 = SliceIterator(i32).init(&array1);
+
+    const array2 = [_]i32{ 9, 8, 7 };
+    var iterator2 = SliceIterator(i32).init(&array2);
+
+    var chain = ChainIterator(@TypeOf(iterator1), @TypeOf(iterator2), i32)
+        .init(&iterator1, &iterator2);
+
+    try std.testing.expectEqual(1, chain.next().?);
+    try std.testing.expectEqual(2, chain.next().?);
+    try std.testing.expectEqual(3, chain.next().?);
+    try std.testing.expectEqual(9, chain.next().?);
+    try std.testing.expectEqual(8, chain.next().?);
+    try std.testing.expectEqual(7, chain.next().?);
+    try std.testing.expect(chain.next() == null);
+}
+
+test "ChainIterator empty" {
+    var iterator1 = EmptyIterator{};
+    var iterator2 = EmptyIterator{};
+
+    var chain = ChainIterator(@TypeOf(iterator1), @TypeOf(iterator2), void)
+        .init(&iterator1, &iterator2);
+
+    try std.testing.expect(chain.next() == null);
+}

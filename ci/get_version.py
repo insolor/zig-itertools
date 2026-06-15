0b303ef989525cb83b0d40fb33bac8d42e3936df
insolor <2442833+insolor@users.noreply.github.com>
2026-06-15 12:09:39 +0300

    Use natizon to get version from build.zig.zon

--- ci/get_version.py
+++ ci/get_version.py
@@ -1,9 +1,19 @@
+# /// script
+# dependencies = [
+#     "natizon==0.2.0",
+# ]
+# requires-python = ">=3.12"
+# ///
+
 from pathlib import Path
-import re
+import natizon
 
 project_root_dir = Path(__file__).parent.parent
-build_zig_zon = project_root_dir / "build.zig.zon"
+build_zig_zon_file = project_root_dir / "build.zig.zon"
 
-build_zig_zon_content = build_zig_zon.read_text()
-match = re.search(r"\.version\s*=\s*\"([\d\.]+)\"", build_zig_zon_content)
-print(match.group(1))
+build_zig_zon = natizon.loads(build_zig_zon_file.read_text())
+version = build_zig_zon.get("version")
+if not version:
+    raise ValueError("Failed to get version from build.zig.zon")
+
+print(version)
